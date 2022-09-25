aff8bc0b26d678557b98728f1c0818ec532ea138
DanielL159 <daneilluizdossantos12@gmail.com>
2022-09-24 22:09:52 -0300

    atualizado

--- Lista_algoritimo_condicional.c
+++ Lista_algoritimo_condicional.c
@@ -540,4 +540,95 @@ int main (){
     
 }*/
 
-//EXERCICIO 18
+//EXERCICIO 18 a ser realizado 
+
+
+//EXERCICIO 19
+/*
+int main (){
+    int i ;
+    float n[4],media,recu,mediarecu;
+    float peso[4]={2.0,3.0,4.0,1.0};
+
+    for ( i = 0; i < 4; i++)
+    {
+       printf("Qual as nota %d do aluno ",i+1);
+       scanf("%f",&n[i]);
+       if (n[i]>10)
+       {
+        n[i] =10;
+       }
+       
+    }
+    media = ((n[0]*peso[0])+(n[1]*peso[1])+(n[2]*peso[2])+(n[3]*peso[3]))/10.0;
+
+    if (media >=7.0)
+    {
+        printf("Media: %.1f\n",media);
+        printf("Aluno aprovado\n");
+    }
+    else if (media <= 5.0)
+    {
+        printf("Media: %.1f\n",media);
+        printf("Aluno reprovado\n");
+
+    }
+    else if(media > 5.0 && media <=6.9)
+    {
+        printf("Aluno em exame.\n");
+        printf("Qual a nota do aluno em exame :");
+        scanf("%f",&recu);
+
+        printf("Nota do exame:%.1f\n",recu);
+        printf("Em exame com media%f",media);
+
+        mediarecu = (recu + media) /2.0;
+
+            if (mediarecu >=5.0)
+        {
+            printf("Aluno aprovado\n");
+            printf("Media final:%.1f",mediarecu);
+        }else if (media < 5.0){
+            printf("Aluno reprovado");
+            printf("Media final:%.1f",mediarecu);
+        }
+    }
+}*/
+
+//EXERCICIO 20
+/*
+int main (){
+    float a,b,c,delta,raiz,raiz2;
+
+    printf("Me forneca o \"A\":");
+    scanf("%f",&a);
+    printf("Me forneca o \"B\":");
+    scanf("%f",&b);
+    printf("Me forneca o \"C\":");
+    scanf("%f",&c);
+
+        if (a== 0)
+        {
+            printf("Nao e equaçao de segundo grau");
+        }else {
+            
+            delta = pow(b,2) - 4 * a * c;
+        if (delta <0 )
+        {
+            printf("Nao existe raiz");
+        }else if (delta == 0)
+        {
+            raiz= (-1*b) /(2*a);
+           printf("raiz unica e com valor de %.2f",raiz) ;
+        }else if (delta >0)
+        {
+            raiz =((-1*b) + sqrt(delta)) /(2*a);
+            raiz2 =((-1*b) - sqrt(delta)) /(2*a);
+            printf("As raizes sao %.1f e %.1f",raiz,raiz2);
+        }
+        
+    
+    }
+
+}*/
+    
