dfb4c389f98f0a87f55ff550283a3cdf9585a047
DanielL159 <danielluizdossantos12@gmail.com>
2022-10-18 12:50:30 -0300

    14

--- Lista_algoritimo_condicional.c
+++ Lista_algoritimo_condicional.c
@@ -404,11 +404,69 @@ int main (){
 }*/
 
 //exercico 14
-/*
+
+
 int main (){
+    int x;
+
+    printf("Me forneca a posicao o mes :");
+    scanf("%d",&x);
+ switch (x)
+ {
+ case 1:
+    printf("Janeiro");
+    break;
+
+ case 2:
+    printf("fevereiro ");
+    break;
+
+case 3:
+    printf("marco");
+    break;
+
+case 4:
+    printf("abriu");
+    break;
+
+case 5:
+    printf("maio");
+    break;
+
+case 6:
+    printf("junho");
+    break;
+    
+case 7:
+    printf("julho");
+    break;
+    
+case 8:
+    printf("agosto");
+    break;
+    
+case 9:
+    printf("setembro");
+    break;
+    
+case 10:
+    printf("outubro");
+    break;
+    
+case 11:
+    printf("novembro");
+    break;
+
+case 12:
+    printf("dezembro");
+    break;
 
 
-}*/
+ default:
+ printf("Mes nao selecionado ou inexistente");
+    break;
+ }   
+}
 
 //exercicio 15
 /*
