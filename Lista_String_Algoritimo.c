9a2d189f21ecc877ae8b4a47e8b6392747e91914
DanielL159 <danielluizdossantos12@gmail.com>
2022-11-22 15:20:35 -0300

    A termianr

---
+++ Lista_String_Algoritimo.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<string.h>
+
+
+/*************************************************************************************************************/
+//Exercicio 1
+/* Elabore um algoritmo que receba o nome completo de um cliente e imprima a quantidade de
+caracteres digitados.*/
+/*
+int main (){
+     int quantidade;
+     char nome[1000];
+
+    printf("Qual o nome:");
+    fgets(nome,1000,stdin);
+
+    quantidade = strlen(nome);
+
+    printf("Total:%d",strlen(nome));
+    printf("Apenas Letras %d",quantidade-1);
+}*/
+
+//Exercicio 2
+/*
+int main (){
+    char cor[8];
+
+    printf("Me fale o nome de uma cor (branco, verde e amarelo.):");
+    scanf("%s",cor);
+
+    if (strcmp(cor,"branco")==0 ||strcmp(cor,"verde")==0 || strcmp(cor,"amarelo")==0 )
+    {
+        printf ("Cor %s validada",cor);
+    }else{
+        printf("Cor invalida");
+    }
+    
+
+}*/
+
+//Exercicio 3
+/*
+
+int main (){
+    char nome[100] , email[100] , endereco[100];
+    char nomeB[100] , emailB[100] , enderecoB[100];
+    
+    printf("Nome: ");
+    fflush(stdin);
+    fgets(nome,100,stdin);
+    
+    printf("email: ");
+    fflush(stdin);
+    fgets(email,100,stdin);
+    
+    printf("endereco: ");
+    fflush(stdin);
+    fgets(endereco,100,stdin);
+    
+    strcpy(nomeB,nome);
+	strcpy(emailB,email);
+	strcpy(enderecoB,endereco);
+
+    printf("Backup feito com sucesso\n");
+        
+}
+*/
+
+//Exercicio 4 
+/*4. Elabore um algoritmo que receba o primeiro nome, o(s) nome(s) do meio, e o ultimo nome do
+usuário. Concatene os dados e imprima o valor final.*/
+/*
+int main (){
+    char nomeIncio[100],nomeMeio[50],nomeFim[50];
+
+    printf("Me forneça o primeiro nome");
+    fflush(stdin);
+    gets(nomeIncio);
+
+    printf("Me forneça o primeiro nome");
+    fflush(stdin);
+    fgets(nomeMeio,50,stdin);
+
+    printf("Me forneça o primeiro nome");
+    fflush(stdin);
+    scanf("%s",&nomeFim);
+
+    strcat(nomeIncio,strcat(nomeMeio,nomeFim));
+    printf("%s",nomeIncio);
+
+}*/
+
+//Exercico 5{tentar na aula}
+/*O usuario informa seu nome, sobrenome e numero do cpf. A partir destes dados é composta sua
+senha de acesso, formada pelo nome, primeira letra do sobrenome e os 3 primeiros números do
+cpf. Elabore um algoritmo que gere e imprima a senha.*/
+
+int main (){
+    int i;
+    char nome[1000],sobrenome[50],cpf[11],cpff[3];
+
+    printf("Nome: ");
+    scanf("%s",&nome);
+
+    printf("Sobrenome: ");
+    scanf("%s",&sobrenome);
+
+    printf("Cpf: ");
+    scanf("%s",&cpf);
+    
+    
+   strcat(nome,sobrenome);
+   strcat(sobrenome,cpf);
+     
+    printf("%s", nome);
+   
+}
--- Lista_algoritimo_repeticao.c
+++ Lista_algoritimo_repeticao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+/************************************************************/
 //EXERCICIO 1 
 /*Faca um programa que leia um número inteiro positivo N e imprima todos os números
 naturais de 0 até N em ordem crescente.*/
@@ -382,7 +384,7 @@ int main (){
   
 }*/
 
-//Exercicio 19(Melhorar)
+//Exercicio 19(Melhorar)****************************************
 /*
 int main(){
     float up=1.0,dow=1.0;
@@ -455,21 +457,6 @@ int main (){
 }*/
 
 // Exercicio 21
-/*Uma pesquisa sobre algumas características físicas da população de uma determinada
-região coletou os seguintes dados, referentes a cada habitante:
-a) sexo (m e f);
-b) cor dos olhos (a (azuis), v (verdes), c (castanhos), p (pretos));
-c) cor dos cabelos (l (loiros), c (castanhos), p (pretos), r (ruivos));
-d) idade em anos.
-e) valor do salário.
-Para o cadastro, todos os campos deve ser validados, ou seja, para sexo, serão aceitos
-somente as opções ‘m’ e ‘f’; para cor de olhos, apenas as opções ‘a’, ‘v’, ‘c’ e ‘p’; para cor
-dos cabelos, apenas as opções ‘l’, ‘c’, ‘p’ e ‘r’; para idade, apenas valores entre 10 e 100
-anos, inclusive estes e para o salário, não aceitar valores negativos.
-Para indicar fim dos habitantes pesquisados, o usuário fornecerá um habitante fictício com
-idade igual a –1.
-Ao final, o algoritmo deve determinar a porcentagem de indivíduos do sexo feminino, com
-idade entre 18 e 35 anos e que tenham olhos castanhos e cabelos castanhos*/
 
 
 #include <string.h>
